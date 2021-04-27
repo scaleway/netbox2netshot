3e5d0f11fcf6c4a96312269cd544287ce5ac9b02
Mathieu Poussin <mpoussin@scaleway.com>
2021-04-27 16:21:39 +0200

    Add basic testing

--- src/netbox.rs
+++ src/netbox.rs
@@ -5,12 +5,14 @@ use std::collections::HashMap;
 
 const APP_USER_AGENT: &str = "netbox2netshot";
 const API_LIMIT: u32 = 100;
+const PATH_PING: &str = "/api/dcim/devices/?name=netbox2netshot-ping";
+const PATH_DCIM_DEVICES: &str = "/api/dcim/devices/";
 
 #[derive(Debug)]
 pub struct NetboxClient {
-    url: String,
-    token: String,
-    client: reqwest::Client,
+    pub url: String,
+    pub token: String,
+    pub client: reqwest::Client,
 }
 
 /// Represent the primary_ip field from the DCIM device API call
@@ -49,6 +51,12 @@ fn extract_offset(url_string: &String) -> Result<u32, Error> {
     }
 }
 
+impl Device {
+    pub fn is_valid(&self) -> bool {
+        self.primary_ip.is_some() && self.name.is_some()
+    }
+}
+
 impl NetboxClient {
     /// Create a client without authentication
     pub fn new_anonymous(url: String) -> Result<Self, Error> {
@@ -75,11 +83,11 @@ impl NetboxClient {
 
     /// Ping the service to make sure it is reachable and pass the authentication (if there is any)
     pub async fn ping(&self) -> Result<bool, Error> {
-        let url = format!("{}/api/dcim/devices/?name=netbox2netshot-ping", self.url);
+        let url = format!("{}{}", self.url, PATH_PING);
         log::debug!("Pinging {}", url);
         let response = self.client.get(url).send().await?;
         log::debug!("Ping response: {}", response.status());
-        Ok(response.status() == 200)
+        Ok(response.status().is_success())
     }
 
     /// Get a single device page
@@ -90,8 +98,8 @@ impl NetboxClient {
         offset: u32,
     ) -> Result<NetboxDCIMDeviceList, Error> {
         let url = format!(
-            "{}/api/dcim/devices/?limit={}&offset={}&{}",
-            self.url, limit, offset, query_string
+            "{}{}?limit={}&offset={}&{}",
+            self.url, PATH_DCIM_DEVICES, limit, offset, query_string
         );
         let page: NetboxDCIMDeviceList = self.client.get(url).send().await?.json().await?;
         Ok(page)
@@ -128,5 +136,125 @@ impl NetboxClient {
 
         log::info!("Fetched {} devices from Netbox", devices.len());
         Ok(devices)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use mockito;
+
+    fn enable_logging() {
+        let _ = simple_logger::SimpleLogger::new().with_level(log::LevelFilter::Debug).init();
+    }
+
+    #[test]
+    fn anonymous_initialization() {
+        enable_logging();
+        let url = mockito::server_url();
+        let client = NetboxClient::new_anonymous(url.clone()).unwrap();
+        assert_eq!(client.token, "");
+        assert_eq!(client.url, url);
+    }
+
+    #[test]
+    fn authenticated_initialization() {
+        enable_logging();
+        let url = mockito::server_url();
+        let token = String::from("hello");
+        let client = NetboxClient::new(url.clone(), token.clone()).unwrap();
+        assert_eq!(client.token, token);
+        assert_eq!(client.url, url);
+    }
+
+    #[tokio::test]
+    async fn failed_ping() {
+        enable_logging();
+        let url = mockito::server_url();
+
+        let _mock = mockito::mock("GET", mockito::Matcher::Any)
+            .with_status(403)
+            .create();
+
+        let client = NetboxClient::new_anonymous(url.clone()).unwrap();
+        let ping = client.ping().await.unwrap();
+        assert_eq!(ping, false);
+    }
+
+    #[tokio::test]
+    async fn successful_ping() {
+        enable_logging();
+        let url = mockito::server_url();
+
+        let _mock = mockito::mock("GET", PATH_PING)
+            .with_body_from_file("tests/data/ping.json")
+            .create();
+
+        let client = NetboxClient::new_anonymous(url.clone()).unwrap();
+        let ping = client.ping().await.unwrap();
+        assert_eq!(ping, true);
+    }
+
+    #[tokio::test]
+    async fn single_good_device() {
+        enable_logging();
+        let url = mockito::server_url();
+
+        let _mock = mockito::mock("GET", PATH_DCIM_DEVICES)
+            .match_query(mockito::Matcher::Any)
+            .with_body_from_file("tests/data/single_good_device.json")
+            .create();
+
+        let client = NetboxClient::new_anonymous(url.clone()).unwrap();
+        let devices = client.get_devices(&String::from("")).await.unwrap();
+
+        assert_eq!(devices.len(), 1);
+
+        let device = devices.first().unwrap();
+
+        assert_eq!(device.name.as_ref().unwrap(), "test-device");
+        assert_eq!(device.id, 1 as u32);
+        assert_eq!(device.primary_ip.as_ref().unwrap().address, "1.2.3.4/32");
+        assert_eq!(device.is_valid(), true);
+    }
+
+    #[tokio::test]
+    async fn single_device_without_primary_ip() {
+        enable_logging();
+        let url = mockito::server_url();
+
+        let _mock = mockito::mock("GET", PATH_DCIM_DEVICES)
+            .match_query(mockito::Matcher::Any)
+            .with_body_from_file("tests/data/single_device_without_primary_ip.json")
+            .create();
+
+        let client = NetboxClient::new_anonymous(url.clone()).unwrap();
+        let devices = client.get_devices(&String::from("")).await.unwrap();
+
+        assert_eq!(devices.len(), 1);
+
+        let device = devices.first().unwrap();
+
+        assert_eq!(device.is_valid(), false);
+    }
+
+    #[tokio::test]
+    async fn single_device_without_name() {
+        enable_logging();
+        let url = mockito::server_url();
+
+        let _mock = mockito::mock("GET", PATH_DCIM_DEVICES)
+            .match_query(mockito::Matcher::Any)
+            .with_body_from_file("tests/data/single_device_without_name.json")
+            .create();
+
+        let client = NetboxClient::new_anonymous(url.clone()).unwrap();
+        let devices = client.get_devices(&String::from("")).await.unwrap();
+
+        assert_eq!(devices.len(), 1);
+
+        let device = devices.first().unwrap();
+
+        assert_eq!(device.is_valid(), false);
     }
 }
